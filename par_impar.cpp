cff302bddd2adf497050da6062f6ca0026548004
douglasDFH <155786486+douglasDFH@users.noreply.github.com>
2024-01-12 21:58:20 -0400

    programa par e impar

---
+++ par_impar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (int arg, char *argv[]){
+	
+	int numero;
+	cout<<"ingrese numero";
+	cin>>numero;
+	if (numero%2)
+		cout<<"el numero:"<<numero<<"es impar"<<endl;
+	else{
+		cout<<"el numero:"<<numero<<"es par"<<endl;
+	}
+	return 0;
+}
